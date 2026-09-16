c90130748539e9dd617d9d245ecbb25cbf242023
KeslerTjava <keslertang@gmail.com>
2026-09-16 12:47:53 -0700

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,15 @@
+public class Data {
+	
+	public static void main(String[] args) {
+	int date = 16;
+	int year = 2026;
+	String month = " September ";
+	String day = " Wednesday";
+	// American format
+	
+	System.out.println("American format:" +day +"," + month + date +"," +" "+ year);
+	System.out.print("European format:" +day +" " + date + month + year);	
+
+		}
+}
+	
